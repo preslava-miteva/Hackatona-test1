8acc27a22a1eec7f3b44a90d74edc7e831c8a765
preslava-miteva <kotemotte@gmail.com>
2025-03-19 18:45:27 +0200

    not done

--- progect2.html/bkend/first-page.js
+++ progect2.html/bkend/first-page.js
@@ -1,14 +1,23 @@
-function search(){
-    let input = document.getElementById("searchbar");
-    input = input.toLowerCase();
-    let x = document.getElementsByClassName('opt');
+document.addEventListener("DOMContentLoaded", function () {
+    const searchButton = document.querySelector(".search-button");
+    const searchBar = document.querySelector(".search-bar");
 
-    for (let i = 0; i < x.length; i++) {
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].style.display = "none";
+    searchButton.addEventListener("click", function () {
+        performSearch();
+    });
+
+    searchBar.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            performSearch();
         }
-        else {
-            x[i].style.display = "list-item";
+    });
+
+    function performSearch() {
+        const query = searchBar.value.trim();
+        if (query) {
+            window.location.href = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
+        } else {
+            alert("Please enter a search query!");
         }
     }
-}
+});
